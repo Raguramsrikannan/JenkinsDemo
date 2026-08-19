bad83acedeaaad5358b3a53092c26cc5989e9805
Raguramsrikannan <raguram1987ag@gmail.com>
2026-08-19 14:35:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins POM Trigger!");
+        System.out.println("Hello from Jenkins POM Trigger Used!");
     }
 }
